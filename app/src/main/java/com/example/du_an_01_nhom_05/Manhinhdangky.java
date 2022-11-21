33a8ece51f3de7b0ca15f5e0cbddf3cd5b8a7980
PhungXuanHuy <huypxph21600@fpt.edu.vn>
2022-11-22 00:00:36 +0700

    đã liên kết với firebase

--- app/src/main/java/com/example/du_an_01_nhom_05/Manhinhdangky.java
+++ app/src/main/java/com/example/du_an_01_nhom_05/Manhinhdangky.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Manhinhdangky extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhdangky);
+
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
