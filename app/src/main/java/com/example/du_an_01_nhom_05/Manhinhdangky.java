197d81de9e17e7438dd1eeb0858dd51d700babf6
PhungXuanHuy <huypxph21600@fpt.edu.vn>
2022-11-21 13:46:26 +0700

    thêm màn hình Đăng nhập và đăng ký

---
+++ app/src/main/java/com/example/du_an_01_nhom_05/Manhinhdangky.java
@@ -0,0 +1,14 @@
+package com.example.du_an_01_nhom_05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Manhinhdangky extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinhdangky);
+    }
+}
